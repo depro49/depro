855990b7a93ea03d42d4758110dddf4830ad23f2
jura_k <yurakvlad@gmail.com>
2019-07-02 18:19:45 +0300

    oK

--- app/src/main/java/com/depro/MainActivity.java
+++ app/src/main/java/com/depro/MainActivity.java
@@ -1,13 +1,11 @@
 package com.depro;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import com.dpcsa.compon.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public String getNameScreen() {
+        return StockDeclareScreens.MAIN;
     }
 }
---
+++ app/src/main/java/com/depro/YouTubeActivity.java
@@ -0,0 +1,11 @@
+package com.depro;
+
+import com.dpcsa.compon.base.BaseActivity;
+
+public class YouTubeActivity extends BaseActivity {
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_youtube;
+    }
+}
